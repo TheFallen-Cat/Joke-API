9ca984933ad60cc41d0691eaf0af9f45a8cd59c5
Abhay Shukla <fallencat.user@gmail.com>
2023-06-02 19:39:54 +0530

    Database Set Up

--- api/app.py
+++ api/app.py
@@ -1,7 +1,20 @@
 from flask import Flask, request, jsonify, render_template
-import dbmanager as db
+from flask_sqlalchemy import SQLAlchemy
+import random
+
+import sqlite3
 
 app = Flask(__name__, template_folder="templates", static_url_path="")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///joke.db"
+
+db = SQLAlchemy(app)
+
+class Joke(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    joke = db.Column(db.String(150), nullable=False)
+
+    def __init__(self, joke):
+        self.joke = joke
 
 
 @app.route("/")
@@ -9,34 +22,30 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/get")
+@app.route("/get/")
 def get_page():
     return render_template("get.html")
 
 
 @app.route("/get/random")
-def random_joke():
-    joke = db.random_joke()
+def return_random_joke():
 
-    req = {}
-    req["id"] = joke[0][0]
-    req["question"] = joke[0][1]
-    req["answer"] = joke[0][2]
+    random_id = random.randrange(1, 4650)
+    with app.app_context():
+        joke = Joke.query.filter_by(id=random_id).first()
 
-    return jsonify(req)
+        return str(joke.joke)
 
 
 @app.route("/get/id/<int:id>")
 def id_joke(id):
-    joke = db.with_id(id)
+    
+    with app.app_context():
+        joke = Joke.query.filter_by(id=id).first()
 
-    req = {}
-    req["id"] = joke[0][0]
-    req["question"] = joke[0][1]
-    req["answer"] = joke[0][2]
-
-    return jsonify(req)
+        return str(joke.joke)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    app.run()
