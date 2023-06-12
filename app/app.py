0a3351f05320c8ec95897b3ccd5796b2d627dfd4
Abhay Shukla <fallencat.user@gmail.com>
2023-06-12 20:24:56 +0530

    Structured folders

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import random
 
 import os
 
-app = Flask(__name__, template_folder="templates", static_url_path="")
+app = Flask(__name__, template_folder="templates", static_url_path="", static_folder="static")
 
 base_directory = os.path.abspath(os.path.dirname(__file__))
 
